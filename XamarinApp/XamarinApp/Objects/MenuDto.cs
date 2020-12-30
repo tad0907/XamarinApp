9f0e9e7aa230dff03c8acd906f374e5565fd58d2
匡史 松本 <tad@viva-life.biz>
2020-12-31 08:39:31 +0900

    プロジェクト ファイルを追加します。

---
+++ XamarinApp/XamarinApp/Objects/MenuDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinApp.Objects
+{
+    public sealed class MenuDto
+    {
+        public MenuDto(string title, string subTitle, string imageName)
+        {
+            Title = title;
+            SubTitle = subTitle;
+            ImageName = imageName;
+        }
+
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string ImageName { get; set; }
+    }
+}
---
+++ XamarinApp/XamarinApp/Pages/MenuPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinApp.Objects;
+
+namespace XamarinApp.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MenuPage : ContentPage
+    {
+        public MenuPage()
+        {
+            InitializeComponent();
+
+            var dtos = new List<MenuDto>();
+
+            dtos.Add(new MenuDto("AAAAA", "aaaaaaaa", "mail.png"));
+            dtos.Add(new MenuDto("BBBBB", "bbbbbbbcccccccccccccccc", "mail.png"));
+
+            MyListView.ItemsSource = dtos;
+        }
+    }
+}
